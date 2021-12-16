a3261a145adeaee6ab91cb407a72865f5cddf4c1
fjsho <gorimura7@gmail.com>
2021-12-16 21:11:17 +0900

    [fix]日本語化の追加

--- application/resources/views/dashboard.blade.php
+++ application/resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h3 class="font-semibold text-xl text-gray-800 leading-tight">
-                        {{ __('Assigned tasks') }}
+                        {{ __('Assigned Tasks') }}
                     </h3>
                 </div>
             </div>
