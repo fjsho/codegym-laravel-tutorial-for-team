18209e8f106a28198defd8a7bfce4e435fb8297f
masayoshixxx <satomasayoshi.xxx@gmail.com>
2022-01-21 21:22:29 +0900

    <fix>create.blade.phpのdivの削除と段落修正

--- application/resources/views/tasks/create.blade.php
+++ application/resources/views/tasks/create.blade.php
@@ -76,11 +76,10 @@
                     </div>
                     <div class="md:w-1/4 px-3 mb-6">
                         <x-label for="actual_time" :value="__('Actual Time')" class="{{ $errors->has('actual_time') ? 'text-red-600' :'' }}" />
-                        <x-textarea id="actual_time" class="block mt-1 w-full {{ $errors->has('actual_time') ? 'border-red-600' :'' }}" type="text" name="actual_time" :value="old('actual_time')" placeholder="例：１、0.25、36" />
+                        <x-input id="actual_time" class="block mt-1 w-full {{ $errors->has('actual_time') ? 'border-red-600' :'' }}" type="text" name="actual_time" :value="old('actual_time')" placeholder="例：1、0.25、36" />
                     </div>
                 </div>
             </div>
-    </div>
-    </form>
+        </form>
     </div>
 </x-app-layout>
